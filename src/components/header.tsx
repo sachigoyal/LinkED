41f6a0c78871a763098a610b0c0195552b80d067
rudrodip <official.rudrodipsarker@gmail.com>
2025-05-01 17:49:10 +0600

    fix weird css issue

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -2,7 +2,7 @@ import ThemeToggler from "@/components/ModeToogle";
 
 export function Header() {
   return (
-    <div className="flex justify-between mt-24">
+    <div className="flex justify-between pt-24">
       <h1 className="text-2xl font-bold font-heading ">LinkED</h1>
       <ThemeToggler />
     </div>
