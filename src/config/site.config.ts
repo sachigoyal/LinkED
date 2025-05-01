56bce7539cf4edfa488b29ef98e78636dbffee25
rudrodip <official.rudrodipsarker@gmail.com>
2025-05-01 17:40:25 +0600

    added site config

---
+++ src/config/site.config.ts
@@ -0,0 +1,32 @@
+import { SiteConfig } from "@/types";
+
+export const siteConfig: SiteConfig = {
+  name: "LinkED",
+  title: "LinkED - Shorten your links",
+  description: "LinkED is a simple URL shortening service that allows you to shorten your links and share them with your friends.",
+  origin: "https://s.sachii.dev",
+  keywords: [
+    "LinkED",
+    "URL Shortener",
+    "URL Shortening Service",
+    "Link Shortener",
+    "Link Shortening Service",
+    "Link Shortener",
+    "Link Shortening Service",
+    "Link Shortener",
+    "Link Shortening Service",
+    "Link Shortener",
+    "Link Shortening Service",
+    "Link Shortener",
+    "Link Shortening Service",
+  ],
+  og: "https://s.sachii.dev/og.png",
+  creator: {
+    name: "sachii",
+    url: "https://sachii.dev",
+  },
+  socials: {
+    github: "https://github.com/sachigoyal/LinkED",
+    x: "https://x.com/sachiigoyal27",
+  }
+}
